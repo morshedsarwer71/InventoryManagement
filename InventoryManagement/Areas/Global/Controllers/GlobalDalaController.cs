f023c7ce6e6c48a55ca39a39ebe507797af9bc50
Morshed Sarwer <morshedsarwer@hotmail.com>
2018-11-14 11:25:49 +0600

    Areas Settings

---
+++ InventoryManagement/Areas/Global/Controllers/GlobalDalaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagement.Areas.Global.Controllers
+{
+    public class GlobalDalaController : Controller
+    {
+        // GET: Global/GlobalDala
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
